fbca081a574aac775048d2ecf0cf618df9523ce3
Anh Quach <anh.quachquituan@halliburton.com>
2021-08-10 14:31:50 +0700

    New approach

--- main.tf
+++ main.tf
@@ -44,7 +44,6 @@ resource "azurerm_network_security_group" "nsg_defined" {
   name = "nsg-${each.key}"
   location = var.location
   resource_group_name = azurerm_resource_group.vnet_rg.name
-  # security_rule = lookup(var.security_rules, var.nsgs[count.index], null)
 
   
   dynamic "security_rule" {
---
+++ output.tf
@@ -0,0 +1,9 @@
+output "subnet_public_id" {
+  description = "ID of public subnet"
+  value = azurerm_subnet.subnet_public.id
+}
+
+output "subnet_private_id" {
+  description = "ID of private subnet"
+  value = azurerm_subnet.subnet_private.id
+}
